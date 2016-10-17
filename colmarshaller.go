00b73f7e53277c13f39dba9fda97633c47ac07d5
Justin Lowery <j@justin.sx>
2016-10-17 01:40:24 +0000

    convert tabs to spaces

--- colmarshaller.go
+++ colmarshaller.go
@@ -1,63 +1,63 @@
 package colmarshaller
 
 import (
-	"encoding/json"
+    "encoding/json"
 
-	"github.com/tidwall/tile38/controller/collection"
-	"github.com/tidwall/tile38/geojson"
+    "github.com/tidwall/tile38/controller/collection"
+    "github.com/tidwall/tile38/geojson"
 )
 
 type PackageCollection struct {
-	*collection.Collection
+    *collection.Collection
 }
 
 type Package struct {
-	Rows   []Row    `json:"rows"`
-	Fields []string `json:"fields"`
+    Rows   []Row    `json:"rows"`
+    Fields []string `json:"fields"`
 }
 
 type Row struct {
-	Id     string    `json:"id"`
-	Obj    []byte    `json:"obj"`
-	Values []float64 `json:"values"`
+    Id     string    `json:"id"`
+    Obj    []byte    `json:"obj"`
+    Values []float64 `json:"values"`
 }
 
 func (c *PackageCollection) MarshalJSON() ([]byte, error) {
 
-	pack := Package{
-		Rows:   make([]Row, c.Count()),
-		Fields: c.FieldArr(),
-	}
-	i := 0
+    pack := Package{
+        Rows:   make([]Row, c.Count()),
+        Fields: c.FieldArr(),
+    }
+    i := 0
 
-	c.Scan(0, false,
-		func(id string, obj geojson.Object, values []float64) bool {
-			objBytes, _ := obj.MarshalJSON()
-			pack.Rows[i] = Row{
-				Id:     id,
-				Obj:    objBytes,
-				Values: values,
-			}
-			i++
-			return true
-		},
-	)
+    c.Scan(0, false,
+        func(id string, obj geojson.Object, values []float64) bool {
+            objBytes, _ := obj.MarshalJSON()
+            pack.Rows[i] = Row{
+                Id:     id,
+                Obj:    objBytes,
+                Values: values,
+            }
+            i++
+            return true
+        },
+    )
     return json.Marshal(pack)
 }
 
 func (c *PackageCollection) UnmarshalJSON(b []byte) (err error) {
 
-	pack := &Package{}
-	if err = json.Unmarshal(b, pack); err != nil {
-		return
-	}
+    pack := &Package{}
+    if err = json.Unmarshal(b, pack); err != nil {
+        return
+    }
 
-	for i := range pack.Rows {
-		obj, err := geojson.ObjectAuto(pack.Rows[i].Obj)
-		if err != nil {
-			return err
-		}
-		c.ReplaceOrInsert(pack.Rows[i].Id, obj, pack.Fields, pack.Rows[i].Values)
-	}
+    for i := range pack.Rows {
+        obj, err := geojson.ObjectAuto(pack.Rows[i].Obj)
+        if err != nil {
+            return err
+        }
+        c.ReplaceOrInsert(pack.Rows[i].Id, obj, pack.Fields, pack.Rows[i].Values)
+    }
     return
 }
